3b200334f049be3772eb1836215be52d6cee8d03
Wolleys <wolleysmigaya@gmail.com>
2023-08-08 08:53:50 +0000

    Extract user credentials from a base64-decoded authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 import re
 import base64
 import binascii
+from typing import Tuple
 
 from .auth import Auth
 
@@ -21,7 +22,7 @@ class BasicAuth(Auth):
             pattern = r'Basic (?P<token>.+)'
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
-                return field_match.groups('token')[0]
+                return field_match.group('token')
         return None
 
     def decode_base64_authorization_header(
@@ -39,3 +40,23 @@ class BasicAuth(Auth):
                 return res.decode('utf-8')
             except binascii.Error:
                 return None
+
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+            ) -> Tuple[str, str]:
+        """Extracts user credentials from a base64-decoded authorization
+        header that uses the Basic authentication flow.
+        """
+        if type(decoded_base64_authorization_header) == str:
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None
