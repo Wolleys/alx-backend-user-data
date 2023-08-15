afc55ea0f635f6623eab41154008ed07397ad24d
Wolleys <wolleysmigaya@gmail.com>
2023-08-15 08:12:27 +0000

    A users function that implements the POST /users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
 """A simple Flask app with user authentication features.
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+from auth import Auth
 
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """GET /
     Return:
-        - the welcome page.
+        - The home page's payload.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """POST /users
+    Return:
+        - The account creation payload.
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
